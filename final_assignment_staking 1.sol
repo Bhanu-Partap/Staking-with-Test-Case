25fa5716a9eadf1abb5655cce6bdd7a95750a18b
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-07-07 12:14:36 +0530

    staking

--- final_assignment_staking 1.sol
+++ final_assignment_staking 1.sol
@@ -1,55 +1,59 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-contract Staking {
+import "./ERC-20.sol";
+
+contract Staking_Token {
 
     ERC20Basic Token;
 
      constructor(ERC20Basic _addressERC20 ){
         Token=_addressERC20;
     }
-    
+
     struct Stake{
         uint stake_amount;
-        uint stake_reward;
         string stake_type;
         uint stake_time;
     }
-    
+
     address mapping_address;
+    uint stake_reward;
+
 
     mapping(address =>Stake) public Stake_details;
-    mapping(address => uint256) public balances;
-
-    event tokensStaked(address from, uint256 amount, uint duration);
+    // mapping(address => uint256) public balances;
 
 
+    event tokensStaked(address from, uint256 amount, uint _duration);
+
+
+    function staking(uint _amount, string memory _type, uint _duration) public  {
+        require(Token.balanceOf(msg.sender)>=_amount,"Insufficient Balance");
+        if(keccak256(abi.encodePacked(_type)) == keccak256(abi.encodePacked("fixed"))){
+            // require(_amount >0," Stake can not be 0 , Enter some amount of tokens");
+            Stake_details[msg.sender].stake_amount = _amount;
+            Stake_details[msg.sender].stake_time = block.timestamp + _duration;
+        Token.transferFrom(msg.sender, address(this), _amount);
+        emit tokensStaked(msg.sender, _amount, block.timestamp);
+
+        }
+
+        else if(keccak256(abi.encodePacked(_type)) == keccak256(abi.encodePacked("unfixed"))){
+            Stake_details[msg.sender].stake_amount = _amount;
+        }
     
-    function staking(uint _amount, string memory _type) public returns(string memory){
-        Stake_details[msg.sender].stake_amount = _amount;
-        Stake_details[msg.sender].stake_type = _type;
-        Stake_details[msg.sender].stake_time = block.timestamp;
-        if(keccak256(abi.encodePacked(_type)) == keccak256(abi.encodePacked("fixed_staking"))){
-            Token.transferFrom(msg.sender, address(this), _amount);
-            balances[msg.sender] = balances[msg.sender]+_amount;
-            emit tokensStaked(msg.sender, _amount,block.timestamp );
 
-        }
-
-        else if(keccak256(abi.encodePacked(_type)) == keccak256(abi.encodePacked("unfixed_staking"))){
-            // Stake_details[msg.sender].stake_amount = _amount;
-            // Stake_details[msg.sender].stake_type = _type;
-            Token.transferFrom(msg.sender, address(this), _amount);
-            balances[msg.sender] = balances[msg.sender]+_amount;
-            emit tokensStaked(msg.sender, _amount);
-
-        }
     }
 
-    // function unstaking()public returns(string memory){
+    function unstaking()public returns(string memory){
 
-    // }
+    }
 
-
+ function getcontractaddress() public returns(address){
+      return address(this);
+      }
 
 }
+
+
